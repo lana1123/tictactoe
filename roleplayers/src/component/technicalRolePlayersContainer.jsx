68bc5c6ee73b3d54d5399e8d5487dc34192813d8
lana wong <lana_1123@hotmail.com>
2020-05-31 19:28:55 +0800

    converting class to function

--- roleplayers/src/component/technicalRolePlayersContainer.jsx
+++ roleplayers/src/component/technicalRolePlayersContainer.jsx
@@ -1,30 +1,52 @@
-import React, { Component } from "react";
+import React from "react";
 import RolePlayersCategoryRow from "./rolePlayersCategoryRow";
 import RolePlayersRole from "./rolePlayersRole";
 
-class TechnicalRolePlayersContainer extends Component {
-  render() {
-    const rows = [];
-    let lastCategory = null;
-    this.props.roleplayers.forEach((roleplayers) => {
-      if (roleplayers.category !== lastCategory) {
-        rows.push(
-          <RolePlayersCategoryRow
-            category={roleplayers.category}
-            key={roleplayers.category}
-          />
-        );
-      }
+function TechnicalRolePlayersContainer({roleplayers}}) {
+  const rows = [];
+  let lastCategory = null;
+  this.props.roleplayers.forEach((roleplayers) => {
+    if (roleplayers.category !== lastCategory) {
       rows.push(
-        <RolePlayersRole
-          roleplayerdetails={roleplayers}
-          key={roleplayers.role}
+        <RolePlayersCategoryRow
+          category={roleplayers.category}
+          key={roleplayers.category}
         />
       );
-      lastCategory = roleplayers.category;
-    });
-    return <div>{rows}</div>;
-  }
+    }
+    rows.push(
+      <RolePlayersRole roleplayerdetails={roleplayers} key={roleplayers.role} />
+    );
+    lastCategory = roleplayers.category;
+  });
+  return <div>{rows}</div>;
 }
 
 export default TechnicalRolePlayersContainer;
+
+// class TechnicalRolePlayersContainer extends Component {
+//   render() {
+//     const rows = [];
+//     let lastCategory = null;
+//     this.props.roleplayers.forEach((roleplayers) => {
+//       if (roleplayers.category !== lastCategory) {
+//         rows.push(
+//           <RolePlayersCategoryRow
+//             category={roleplayers.category}
+//             key={roleplayers.category}
+//           />
+//         );
+//       }
+//       rows.push(
+//         <RolePlayersRole
+//           roleplayerdetails={roleplayers}
+//           key={roleplayers.role}
+//         />
+//       );
+//       lastCategory = roleplayers.category;
+//     });
+//     return <div>{rows}</div>;
+//   }
+// }
+
+// export default TechnicalRolePlayersContainer;
