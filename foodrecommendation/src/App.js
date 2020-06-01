bb0a60b1ca5b186c0903a74757a5ee8a4ee633d3
lana wong <lana_1123@hotmail.com>
2020-06-01 17:39:41 +0800

    move states up

--- foodrecommendation/src/App.js
+++ foodrecommendation/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import FoodList from "./components/foodList";
 import NavBar from "./components/navbar";
 import RandomPick from "./components/randomPick";
@@ -9,19 +9,89 @@ import {
   Redirect,
 } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <NavBar />
-      <header className="App-header">
-        <Switch>
-          <Route path="/" exact component={RandomPick} />
-          <Route path="/foodlist" component={FoodList} />
-          <Route path="/randompick" component={RandomPick} />
-        </Switch>
-      </header>
-    </Router>
-  );
+class App extends Component {
+  state = {
+    //declaring a state where items containing text and key
+    food: [],
+    currentFood: {
+      text: "",
+      key: "",
+    },
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      currentFood: {
+        text: e.target.value,
+        key: Date.now(),
+      },
+    });
+  };
+
+  addFood = (e) => {
+    e.preventDefault();
+    const newFav = this.state.currentFood;
+    if (newFav.text !== "") {
+      const newFavs = [...this.state.food, newFav];
+      this.setState({
+        food: newFavs,
+        currentFood: {
+          text: "",
+          key: "",
+        },
+      });
+    }
+  };
+
+  deleteFood = (key) => {
+    const filteredFavFood = this.state.food.filter((food) => food.key !== key);
+    this.setState({
+      food: filteredFavFood,
+    });
+  };
+
+  updateFood = (text, key) => {
+    const favFoods = this.state.food;
+    favFoods.map((food) => {
+      if (food.key === key) {
+        food.text = text;
+      }
+    });
+    this.setState({
+      food: favFoods,
+    });
+  };
+
+  render() {
+    return (
+      <Router>
+        <NavBar />
+        <header className="App-header">
+          <Switch>
+            <Route path="/" exact component={RandomPick} />
+            <Route
+              path="/foodlist"
+              component={() => (
+                <FoodList
+                  food={this.state.food}
+                  updateFood={this.updateFood}
+                  addFood={this.addFood}
+                  deleteFood={this.deleteFood}
+                  handleInput={this.handleInput}
+                  currentFood={this.state.currentFood}
+                />
+              )}
+            />
+            <Route path="/randompick" component={RandomPick} />
+          </Switch>
+        </header>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- foodrecommendation/src/components/foodList.js
+++ foodrecommendation/src/components/foodList.js
@@ -8,79 +8,33 @@ import TotalFavFood from "./totalFavFood";
 library.add(faTrash); //to add the trash icon into the library
 
 class FoodList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      //declaring a state where items containing text and key
-      food: [],
-      currentFood: {
-        text: "",
-        key: "",
-      },
-    };
-  }
-
-  handleInput = (e) => {
-    this.setState({
-      currentFood: {
-        text: e.target.value,
-        key: Date.now(),
-      },
-    });
-  };
-
-  addFood = (e) => {
-    e.preventDefault();
-    const newFav = this.state.currentFood;
-    if (newFav.text !== "") {
-      const newFavs = [...this.state.food, newFav];
-      this.setState({
-        food: newFavs,
-        currentFood: {
-          text: "",
-          key: "",
-        },
-      });
-    }
-  };
-
-  deleteFood = (key) => {
-    const filteredFavFood = this.state.food.filter((food) => food.key !== key);
-    this.setState({
-      food: filteredFavFood,
-    });
-  };
-
-  updateFood = (text, key) => {
-    const favFoods = this.state.food;
-    favFoods.map((food) => {
-      if (food.key === key) {
-        food.text = text;
-      }
-    });
-    this.setState({
-      food: favFoods,
-    });
-  };
-
   render() {
+    const {
+      food,
+      addFood,
+      deleteFood,
+      handleInput,
+      currentFood,
+      updateFood,
+    } = this.props;
+
     return (
       <div className="favFoodInput">
         What's your favourite food?
-        <form id="fav-food-form" onSubmit={this.addFood}>
+        <form id="fav-food-form" onSubmit={addFood}>
           <input
             type="text"
             placeholder="Enter your favourite food..."
-            value={this.state.currentFood.text}
-            onChange={this.handleInput}
+            value={currentFood.text}
+            onChange={handleInput}
           />
           <button type="submit">Add</button>
         </form>
-        <TotalFavFood totalFood={this.state.food.length} />
+        <TotalFavFood totalFood={food.length} />
         <ListFavFood
-          food={this.state.food}
-          deleteFood={this.deleteFood}
-          updateFood={this.updateFood}
+          food={food}
+          deleteFood={deleteFood}
+          updateFood={updateFood}
         />
       </div>
     );
