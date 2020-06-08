2005dec84b3532a2946071177f121a28cf7a1456
lana wong <lana_1123@hotmail.com>
2020-06-08 17:30:04 +0800

    basic published version

--- foodrecommendation/src/components/randomPickResults.js
+++ foodrecommendation/src/components/randomPickResults.js
@@ -3,19 +3,14 @@ import "./randomPickResults.css";
 
 //Child list items component / function
 function RandomPickResults({ pickedFood }) {
-  const listRandomPickResults =
-    pickedFood &&
-    pickedFood.map((food) => {
-      return (
-        <div className="randomPickResults">
-          <div key={food.text}>{food.text}</div>
-        </div>
-      );
-    });
-
   return (
-    <div>
-      <div className="randomPickResultsPage">{listRandomPickResults}</div>
+    <div className="randomPickResultsPage">
+      <div>
+        {pickedFood &&
+          pickedFood.map((food) => (
+            <div className="randomPickResults">{food.text}</div>
+          ))}
+      </div>
     </div>
   );
 }
