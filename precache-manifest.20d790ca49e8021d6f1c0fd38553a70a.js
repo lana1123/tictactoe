2ce741e93ffa24a81920c29d0f7b86271f3dda23
lana wong <lana_1123@hotmail.com>
2020-06-19 10:09:58 +0800

    Updates

---
+++ precache-manifest.20d790ca49e8021d6f1c0fd38553a70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86a6fb3f6128f45cd30ddf2d7856bdb",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "c901c4d4fe63312e8e12",
+    "url": "/tictactoe/static/css/main.a60244ce.chunk.css"
+  },
+  {
+    "revision": "020cc16b7f377f1ffe28",
+    "url": "/tictactoe/static/js/2.67b1865c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.67b1865c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c901c4d4fe63312e8e12",
+    "url": "/tictactoe/static/js/main.fed50bf0.chunk.js"
+  },
+  {
+    "revision": "9bb9993f2b0b25e9f137",
+    "url": "/tictactoe/static/js/runtime-main.42461ed6.js"
+  }
+]);
